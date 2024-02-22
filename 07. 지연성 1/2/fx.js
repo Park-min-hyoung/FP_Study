c919dc4d303aff4df848d7ff5970f568eb5a554e
Park-min-hyoung <qkralswgud1324@gmail.com>
2024-02-22 09:11:14 +0900

    지연성 1(2(진행중))

---
+++ 07. 지연성 1/2/fx.js
@@ -0,0 +1,52 @@
+const log = console.log;
+
+const go = (...args) => reduce((acc, f) => f(acc), args);
+const pipe =
+  (f, ...fs) =>
+  (...args) =>
+    go(f(...args), ...fs);
+const curry =
+  (f) =>
+  (a, ..._) =>
+    _.length ? f(a, ..._) : (..._) => f(a, ..._);
+
+// const map = curry((f, iter) => {
+//   const res = [];
+//   for (const a of iter) {
+//     res.push(f(a));
+//   }
+
+//   return res;
+// });
+
+// const filter = curry((f, iter) => {
+//   const res = [];
+//   for (const a of iter) {
+//     if (f(a)) res.push(a);
+//   }
+
+//   return res;
+// });
+
+// const reduce = curry((f, acc, iter) => {
+//   if (!iter) {
+//     iter = acc[Symbol.iterator]();
+//     acc = iter.next().value;
+//   }
+//   for (const a of iter) {
+//     acc = f(acc, a);
+//   }
+
+//   return acc;
+// });
+
+// const range = (l, iter) => {
+//   let i = -1;
+//   const res = [];
+//   while (++i < l) {
+//     res.push(i);
+//   }
+//   return res;
+// };
+
+const L = {};
